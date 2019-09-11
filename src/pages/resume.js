33e0d2156241bf128a9776b6f4652c92a0df62a6
Hanyu Xu <xhyalex@gmail.com>
2019-09-11 18:20:10 -0400

    update

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'gatsby'
-import Layout from '../layout/layout'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "gatsby";
+import Layout from "../layout/layout";
 
-import SEO from '../components/SEO'
-import SuperText from '../components/SuperText'
+import SEO from "../components/SEO";
+import SuperText from "../components/SuperText";
 
 const Center = styled.div`
   height: 80vh;
@@ -12,20 +12,19 @@ const Center = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-`
+`;
 
 const ResumePage = () => {
   return (
     <Layout>
-      <SEO title='FOUR OH FOUR!' />
-      <Center>
-        <SuperText size='3em'>FourOhFour</SuperText>
-        <p>Still working on this page. Coming Soon.</p>
-        <Link to='/'>Go Home</Link>
-      </Center>
+      <SEO title="Resume" />
+      <SuperText size="3em" id="resume-title">
+        Resume
+      </SuperText>
+      <p>Still working on this page. Coming Soon.</p>
+      <Link to="/">Go Home</Link>
     </Layout>
-  )
-}
+  );
+};
 
-export default ResumePage
-
+export default ResumePage;
